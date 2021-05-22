33e642e8b0dcbfd27b55203bea2d4a7adee52c3e
Jakob Persons <jakobep24@gmail.com>
2021-05-22 11:37:43 -0400

    changes to assessment component

--- associate-frontend/src/components/Assessment.js
+++ associate-frontend/src/components/Assessment.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Assessment = () => {
+
+	const [formData, setFormData] = useState({
+		reflection: "",
+		date: ""
+	})
+	console.log(formData)
+
+	function reflectionChange(e) {
+		setFormData({ ...formData, [e.target.name]: e.target.value})
+	}
+
+
   return (
 		<div className="assessment">
 			<h4>Assessment Center</h4>
@@ -10,6 +22,26 @@ const Assessment = () => {
 				<li>Adjustments</li>
 				<li>Actions</li>
 			</ul>
+			<div className="form-container">
+				<form className="reflection-form">
+					<label>Date</label>
+					<input
+						type="date"
+						name="date"
+						value={formData.date}
+						onChange={reflectionChange}
+					/>
+					<label>Weekly Reflection: </label>
+					<input
+						type="text"
+						name="reflection"
+						value={formData.reflection}
+						onChange={reflectionChange}
+					/>
+					<input type="submit" />
+				</form>
+				<p>Assessment form will go here</p>
+			</div>
 		</div>
 	);
 }
